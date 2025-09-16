88c8dec29429214b9513765154e3d9492b201805
Nidhi Sharma <nidhiiisharma2021@gmail.com>
2025-09-17 02:29:12 +0530

    added client side form validation

--- app.js
+++ app.js
@@ -9,18 +9,21 @@ app.set("views", path.join(__dirname, "/views"));
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.static(path.join(__dirname, "/public")));
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(methodOverride("_method"));
-app.engine('ejs', ejsMate);
+app.engine("ejs", ejsMate);
 
 const mongoose = require("mongoose");
 
-main().then(() =>{console.log("connected");})
-.catch(err => console.log(err));
+main()
+  .then(() => {
+    console.log("connected");
+  })
+  .catch((err) => console.log(err));
 
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/airbnb');
+  await mongoose.connect("mongodb://127.0.0.1:27017/airbnb");
 }
 
 // const updateimg = async() =>{
@@ -32,61 +35,65 @@ async function main() {
 
 // updateimg();
 
-
-
-app.get("/", (req,res)=>{
-  res.send("HII, it's working!")
-})
+app.get("/", (req, res) => {
+  res.send("HII, it's working!");
+});
 
 //shows all the list (home route)
-app.get("/listings", async(req,res)=>{
+app.get("/listings", async (req, res) => {
   let allListings = await Listing.find({});
-  res.render("./listings/index.ejs", {allListings});
-})
+  res.render("./listings/index.ejs", { allListings });
+});
 
 //creating a form to create new list
-app.get("/listings/new", (req,res)=>{
+app.get("/listings/new", (req, res) => {
   res.render("./listings/new.ejs");
-})
+});
 
 //create new list
-app.post("/listings", async(req,res)=>{
+app.post("/listings", async (req, res) => {
   let listing = req.body.listing;
   let newListing = new Listing(req.body.listing);
   await newListing.save();
   res.redirect("/listings");
-})
+});
 
-
-//shows a particular list by searching with findbyid. 
-app.get("/listings/:id", async(req,res)=>{
+//shows a particular list by searching with findbyid.
+app.get("/listings/:id", async (req, res) => {
   let { id } = req.params;
   const listing = await Listing.findById(id);
   res.render("./listings/show.ejs", { listing });
-})
+});
 
 //renders an update form
-app.get("/listings/:id/edit", async(req,res)=>{
+app.get("/listings/:id/edit", async (req, res) => {
   let { id } = req.params;
   const listing = await Listing.findById(id);
   res.render("./listings/edit.ejs", { listing });
-})
+});
 
 //update route
-app.put("/listings/:id", async(req,res) =>{
+app.put("/listings/:id", async (req, res) => {
   let { id } = req.params;
-  await Listing.findByIdAndUpdate(id, {...req.body.listing});
+  const listing = await Listing.findById(id);
+
+  if (!req.body.listing.url) {
+  req.body.listing.url = "https://images.unsplash.com/photo-1571896349842-33c89424de2d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8aG90ZWxzfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60";
+  }
+
+  listing.set(req.body.listing);
+  await listing.save();
   res.redirect(`/listings/${id}`);
-})
+});
+
 
 //deleting a list
-app.delete("/listings/:id", async(req,res) =>{
+app.delete("/listings/:id", async (req, res) => {
   let { id } = req.params;
   let deletedList = await Listing.findByIdAndDelete(id);
   res.redirect("/listings");
-  console.log(deletedList);  
-})
-
+  console.log(deletedList);
+});
 
 // app.get("/testListing", async(req,res)=>{
 //   let sampleListing = new Listing({
@@ -102,6 +109,6 @@ app.delete("/listings/:id", async(req,res) =>{
 //   res.send("saved!");
 // })
 
-app.listen(1111, ()=>{
-    console.log("Server is listening at port 1111");    
-})
+app.listen(1111, () => {
+  console.log("Server is listening at port 1111");
+});
